40dc59ad8dc96cef169b294be6ffa8db87331e13
Danick <45609733+RestartFU@users.noreply.github.com>
2022-01-04 21:11:27 -0500

    added EnergyEffectItem interface

--- item.go
+++ item.go
@@ -1,6 +1,7 @@
 package class
 
 import (
+	"github.com/df-mc/dragonfly/server/entity/effect"
 	"github.com/df-mc/dragonfly/server/player"
 	"github.com/df-mc/dragonfly/server/world"
 )
@@ -8,5 +9,10 @@ import (
 type EnergyUseItem interface {
 	Energy() int
 	Use(p *player.Player)
+	Item() world.Item
+}
+type EnergyEffectItem interface {
+	Energy() int
+	Effect() effect.Effect
 	Item() world.Item
 }
